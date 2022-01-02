e7cd0a6564d0bbd09e608700867e5608afb4ed6d
Ville Takanen <vitkukissa@gmail.com>
2022-01-02 21:43:52 +0200

    feat: added pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { createPinia } from 'pinia'
 import { routes } from './views/routes'
 import App from './App.vue'
 
@@ -8,7 +9,10 @@ const router = createRouter({
   routes: routes
 })
 
+const pinia = createPinia()
+
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Home</div>
+  <div>Home ({{ init }})</div>
 </template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import { useStore } from '../stores/main'
+
+export default defineComponent({
+  setup() {
+    const store = useStore()
+    return {
+      init: computed(() => store.initialized),
+    }
+  }
+})
+</script>
