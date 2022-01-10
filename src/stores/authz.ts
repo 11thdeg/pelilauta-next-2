a34cdca7551f5eb62a083fd3eb0e7bdae7660147
Ville Takanen <vitkukissa@gmail.com>
2022-01-10 10:10:03 +0200

    fix: correct import for account object

--- src/stores/authz.ts
+++ src/stores/authz.ts
@@ -1,25 +1,7 @@
 import { defineStore } from 'pinia'
 import { computed, reactive, useTransitionState } from 'vue'
 import type { User } from 'firebase/auth'
-
-class Account {
-
-  email: string
-  displayName: string
-  photoURL: string
-  uid: string
-  isAnonymous: boolean
-  providerId: string
-
-  constructor(user: User | null) {
-    this.email = user?.email || ''
-    this.displayName = user?.displayName || ''
-    this.photoURL = user?.photoURL || ''
-    this.uid = user?.uid || ''
-    user && !user.isAnonymous ? this.isAnonymous = false : this.isAnonymous = true
-    this.providerId = user?.providerId || ''
-  }
-}
+import { Account } from '../skaldstore/src'
 
 export const useAuthz = defineStore('session', () => {
   const state = reactive({
