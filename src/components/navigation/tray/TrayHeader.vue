c9b771e634074cfb9b0d97661753ea5f2c8d03a2
Ville Takanen <vitkukissa@gmail.com>
2022-03-08 14:52:29 +0200

    fix: tray header links work

--- src/components/navigation/tray/TrayHeader.vue
+++ src/components/navigation/tray/TrayHeader.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import Icon from '../../ui/Icon.vue'
 import SpacerDiv from '../../ui/SpacerDiv.vue'
 
@@ -18,6 +18,7 @@ const emit = defineEmits<{
 }>()
 
 const route = useRoute()
+const router = useRouter()
 const selected = computed(() => props.active || props.to === route.fullPath)
 
 function moveUp () {
@@ -28,30 +29,25 @@ function moveDown () {
   emit('moveDown')
 }
 
+function reroute () {
+  if (props.to && props.to !== route.fullPath) {
+    router.push(props.to)
+  }
+}
+
 </script>
 
 <template>
   <div
     class="TrayHeader"
     :class="{ selected: selected }"
+    @click.prevent="reroute"
   >
-    <router-link
-      v-if="to"
-      :to="to"
-    >
-      <Icon
-        v-if="icon"
-        :icon="icon"
-      />
-      <h3>{{ label }}</h3>
-    </router-link>
-    <template v-else>
-      <Icon
-        v-if="icon"
-        :icon="icon"
-      />
-      <h3>{{ label }}</h3>
-    </template>
+    <Icon
+      v-if="icon"
+      :icon="icon"
+    />
+    <h3>{{ label }}</h3>
     <template v-if="tools">
       <SpacerDiv />
       <Icon
@@ -71,6 +67,7 @@ function moveDown () {
 <style scoped>
 div.TrayHeader {
   display: flex;
+  gap: var(--page-grid-gap);
 }
 div.TrayHeader a {
   color: var(--color-high);
