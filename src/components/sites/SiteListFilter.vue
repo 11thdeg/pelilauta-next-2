e00e2c2e338633344ec7ded1c3e8bad40c29b6c2
Ville Takanen <vitkukissa@gmail.com>
2022-03-22 08:45:23 +0200

    feat: initial site list ordering filter

---
+++ src/components/sites/SiteListFilter.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import Button from '../ui/Button.vue'
+import ActionBar from '../ui/ActionBar.vue';
+
+const emit = defineEmits<{
+  (e: 'order_date_desc'): void
+  (e: 'order_name_asc'): void
+}>()
+</script>
+
+<template>
+  <ActionBar class="SiteListFilter">
+    <h4>ORDER_BY</h4>
+    <Button text icon="eye" @click="emit('order_date_desc')">
+      BY_DATE
+    </Button>
+    <Button text icon="eye" @click="emit('order_name_asc')">
+      BY_NAME
+    </Button>
+  </ActionBar>
+</template>
+
+<style lang="sass" scoped>
+</style>
--- src/views/sites/siteindex/OwnedSubview.vue
+++ src/views/sites/siteindex/OwnedSubview.vue
@@ -1,24 +1,35 @@
 <script lang="ts" setup>
-import { computed, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { useSites } from '../../../composables/useSites'
 import { useAuthz } from '../../../stores/authz'
 import Card from '../../../components/ui/Card.vue'
 import CardStreamSection from '../../../components/layout/CardStreamSection.vue'
+import SiteListFilter from '../../../components/sites/SiteListFilter.vue'
 
 const { userSites, subscribeToUserSites } = useSites()
 const authz = useAuthz()
+const orderBy=ref('name_asc')
 
 watch(() => authz.user, (user) => {
   subscribeToUserSites(user.uid)
 }, { immediate: true })
 
 const sites = computed(() => {
-  return userSites.value
+  const arr = userSites.value
+  if (orderBy.value === 'name_asc') {
+    return arr.sort((a, b) => a.name > b.name ? 1 : -1)
+  }
+  return arr.sort((a, b) => a.compareFlowTime(b))
 })
+
 
 </script>
 <template>
   <article class="OwnedSubView">
+    <SiteListFilter
+      @order_date_desc="orderBy = 'date_desc'"
+      @order_name_asc="orderBy ='name_asc'"
+    />
     <CardStreamSection>
       <Card
         v-for="site in sites"
